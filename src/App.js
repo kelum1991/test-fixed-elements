126233a988ba84686e1b8a5fecb29d407d147dfa
Kelum Chathuranga <kelum.chathuranga@nzsafetyblackwoods.co.nz>
2021-08-05 13:58:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-wrap">
+        
+     <form>
+  <label for="fname">Username:</label>
+  <input type="text" id="fname" name="fname" />
+  <label for="lname">Password:</label>
+  <input type="password" id="lname" name="lname" />
+  <input type="submit" value="Submit" />
+</form>
+</div>
+      <span className="fixed-element"></span>
     </div>
   );
 }
